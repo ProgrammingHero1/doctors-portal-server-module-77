61a01c6799cb5e559a0cfbad75dc0d26ff9a0da5
Programming Hero <programming.hero1@gmail.com>
2022-11-15 21:02:49 -0600

    save user info to the database

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     try {
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
 
         // Use Aggregate to query multiple collection and then merge data
         app.get('/appointmentOptions', async (req, res) => {
@@ -94,23 +95,37 @@ async function run() {
          * app.delete('/bookings/:id')
         */
 
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
             const query = {
                 appointmentDate: booking.appointmentDate,
                 email: booking.email,
-                treatment: booking.treatment 
+                treatment: booking.treatment
             }
 
             const alreadyBooked = await bookingsCollection.find(query).toArray();
 
-            if (alreadyBooked.length){
+            if (alreadyBooked.length) {
                 const message = `You already have a booking on ${booking.appointmentDate}`
-                return res.send({acknowledged: false, message})
+                return res.send({ acknowledged: false, message })
             }
 
             const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
 
