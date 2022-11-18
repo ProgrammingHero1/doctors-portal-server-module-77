e52a64405e3ddda6c30982eb2013034d6b5492b1
Programming Hero <programming.hero1@gmail.com>
2022-11-17 19:58:11 -0600

    get and post api for doctors

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ async function run() {
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
         const usersCollection = client.db('doctorsPortal').collection('users');
+        const doctorsCollection = client.db('doctorsPortal').collection('doctors');
 
         // Use Aggregate to query multiple collection and then merge data
         app.get('/appointmentOptions', async (req, res) => {
@@ -205,6 +206,18 @@ async function run() {
                 }
             }
             const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+        app.get('/doctors', async(req, res) =>{
+            const query = {};
+            const doctors = await doctorsCollection.find(query).toArray();
+            res.send(doctors); 
+        })
+
+        app.post('/doctors', async(req, res) =>{
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
             res.send(result);
         })
 
